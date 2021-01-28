f8ac6423151604fcf851822e61df9c6cbc7e9c99
Phil <phil@hive.properties>
2021-01-29 08:51:08 +1100

    some utils.

---
+++ src/utils/domUtils.js
@@ -0,0 +1,25 @@
+export function getComputedStyle(element, value) {
+  const s = window.getComputedStyle(element);
+  return s.getPropertyValue(value);
+}
+export function pauseAllVideo(element) {
+  const videos = element.querySelectorAll("video"); // get all videos using "video" css selector
+  /*Loop*/
+  for (let video of videos) {
+    // use var video if "strict mode" is enabled
+    video.pause();
+  }
+}
+export function playAllVideo(element) {
+  const videos = element.querySelectorAll("video"); // get all videos using "video" css selector
+  /*Loop*/
+  for (let video of videos) {
+    // use var video if "strict mode" is enabled
+    video.play();
+  }
+}
+export function setStyle(element, obj) {
+  for (const [key, value] of Object.entries(obj)) {
+    element.style[key] = value;
+  }
+}
